283157ef3575d55d7053374bee939d3b2276a1d7
KonH <konhit@gmail.com>
2025-05-02 20:25:33 +0200

    Add VContainer & custom Arch.Unity

---
+++ Assets/Scripts/Bootstrap/GameLifetimeScope.cs
@@ -0,0 +1,12 @@
+﻿using VContainer;
+using VContainer.Unity;
+using Arch.Unity;
+
+namespace Bootstrap {
+	public sealed class GameLifetimeScope : LifetimeScope {
+		protected override void Configure(IContainerBuilder builder) {
+			builder.UseNewArchApp(Lifetime.Scoped, c => {
+			});
+		}
+	}
+}
