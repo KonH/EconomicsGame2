13e3476fca126151ad7425d687360683166ce31c
KonH <konhit@gmail.com>
2025-05-04 10:10:08 +0200

    Update camera position using world position

--- Assets/Scripts/Systems/MouseDragScrollCameraSystem.cs
+++ Assets/Scripts/Systems/MouseDragScrollCameraSystem.cs
@@ -14,7 +14,7 @@ namespace Systems {
 
 		readonly QueryDescription _cameraQueryDescription =
 			new QueryDescription()
-				.WithAll<CameraReference>();
+				.WithAll<CameraReference, WorldPosition>();
 
 		public MouseDragScrollCameraSystem(World world, CameraScrollSettings cameraScrollSettings) : base(world) {
 			_cameraScrollSettings = cameraScrollSettings;
@@ -22,13 +22,13 @@ namespace Systems {
 
 		public override void Update(in SystemState systemState) {
 			var state = systemState;
-			World.Query(_dragQueryDescription, (Entity entity, ref MouseButtonHold mouseButtonHold, ref MouseDrag mouseDrag) => {
+			World.Query(_dragQueryDescription, (Entity _, ref MouseButtonHold mouseButtonHold, ref MouseDrag mouseDrag) => {
 				if (mouseButtonHold.Button != _cameraScrollSettings.TargetButton) {
 					return;
 				}
 				var delta = mouseDrag.Delta;
-				World.Query(_cameraQueryDescription, (Entity cameraEntity, ref CameraReference cameraReference) => {
-					cameraReference.Camera.transform.position -= new Vector3(delta.x, delta.y, 0) * _cameraScrollSettings.DragSpeed * state.DeltaTime;
+				World.Query(_cameraQueryDescription, (Entity _, ref CameraReference _, ref WorldPosition worldPosition) => {
+					worldPosition.Position -= new Vector2(delta.x, delta.y) * _cameraScrollSettings.DragSpeed * state.DeltaTime;
 				});
 			});
 		}
