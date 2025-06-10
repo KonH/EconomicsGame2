a6d9e7e37d3b6e96c852880249cb8ad001f20b11
KonH <konhit@gmail.com>
2025-06-10 19:46:46 +0200

    Add menu item for deleting saves

---
+++ Assets/Scripts/Editor/PersistentStateMenuItems.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace Editor {
+	public sealed class PersistentStateMenuItems {
+		[MenuItem("EconomicsGame/State/DeleteAll")]
+		static void DeleteAllSaves() {
+			var saveRoot = Application.persistentDataPath;
+			var saveFiles = Directory.GetFiles(saveRoot, "*.json");
+
+			if (saveFiles.Length == 0) {
+				Debug.Log("No save files found to delete.");
+				return;
+			}
+
+			var count = 0;
+			foreach (var file in saveFiles) {
+				File.Delete(file);
+				count++;
+				Debug.Log($"Deleted save file: {Path.GetFileName(file)}.");
+			}
+
+			Debug.Log($"Successfully deleted {count} save files from {saveRoot}.");
+		}
+	}
+}
