d3b82d4b77535d62e83c674a249b340e6a304f0a
KonH <konhit@gmail.com>
2025-06-10 20:51:22 +0200

    Add example system to move directly to clicked cell

--- Assets/Scripts/Bootstrap/GameLifetimeScope.cs
+++ Assets/Scripts/Bootstrap/GameLifetimeScope.cs
@@ -41,6 +41,7 @@ namespace Bootstrap {
 				c.Add<MouseDragScrollCameraSystem>();
 				c.Add<CellClickSystem>();
 				c.Add<KeyboardMovementSystem>();
+				c.Add<DirectCellMovementSystem>();
 				c.Add<CellMovementSystem>();
 				c.Add<MovementSystem>();
 				c.Add<ActionProgressSystem>();
---
+++ Assets/Scripts/Systems/DirectCellMovementSystem.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using Arch.Core;
+using Arch.Unity.Toolkit;
+using Components;
+using Configs;
+
+namespace Systems {
+	public sealed class DirectCellMovementSystem : UnitySystemBase {
+		readonly QueryDescription _clickedCellsQuery = new QueryDescription()
+			.WithAll<Cell, CellClick>();
+
+		readonly QueryDescription _movableEntitiesQuery = new QueryDescription()
+			.WithAll<IsManualMovable, OnCell>()
+			.WithNone<MoveToCell>();
+
+		readonly MovementSettings _movementSettings;
+
+		public DirectCellMovementSystem(World world, MovementSettings movementSettings) : base(world) {
+			_movementSettings = movementSettings;
+		}
+
+		public override void Update(in SystemState _) {
+			var targetCell = GetClickedCellPosition();
+			if (targetCell == null) {
+				return;
+			}
+
+			World.Query(_movableEntitiesQuery, (Entity entity, ref OnCell cellPosition) => {
+				if (cellPosition.Position == targetCell.Value) {
+					return;
+				}
+
+				World.Add(entity, new MoveToCell {
+					OldPosition = cellPosition.Position,
+					NewPosition = targetCell.Value
+				});
+
+				World.Add(entity, new StartAction {
+					Speed = _movementSettings.Speed
+				});
+
+				Debug.Log($"Moving entity from {cellPosition.Position} to {targetCell.Value}");
+			});
+
+			World.Query(_clickedCellsQuery, (cellEntity) => {
+				World.Remove<CellClick>(cellEntity);
+			});
+		}
+
+		Vector2Int? GetClickedCellPosition() {
+			Vector2Int? clickedCellPos = null;
+
+			World.Query(_clickedCellsQuery, (Entity cellEntity, ref Cell cell) => {
+				clickedCellPos = cell.Position;
+			});
+
+			return clickedCellPos;
+		}
+	}
+}
