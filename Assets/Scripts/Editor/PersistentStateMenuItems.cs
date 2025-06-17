67f944826bda20abdd8db108d6f438ba43055db7
KonH <konhit@gmail.com>
2025-06-17 08:25:06 +0200

    Add option to open save directory

--- Assets/Scripts/Editor/PersistentStateMenuItems.cs
+++ Assets/Scripts/Editor/PersistentStateMenuItems.cs
@@ -23,5 +23,25 @@ namespace Editor {
 
 			Debug.Log($"Successfully deleted {count} save files from {saveRoot}.");
 		}
+
+		[MenuItem("EconomicsGame/State/OpenSavesDirectory")]
+		static void OpenSavesDirectory() {
+			var saveRoot = Path.Combine(Application.persistentDataPath);
+
+			if (!Directory.Exists(saveRoot)) {
+				Debug.LogError($"Save directory doesn't exist: {saveRoot}");
+				return;
+			}
+
+			var saveFiles = Directory.GetFiles(saveRoot, "*.json");
+			foreach (var file in saveFiles) {
+				Debug.Log($"Opening file manager for save file: {Path.GetFileName(file)}");
+				EditorUtility.RevealInFinder(file);
+				return;
+			}
+
+			Debug.Log($"Opening file manager for save directory: {saveRoot}");
+			EditorUtility.RevealInFinder(saveRoot);
+		}
 	}
 }
