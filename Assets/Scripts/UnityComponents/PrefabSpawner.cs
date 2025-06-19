da067d54d31eeec8ded1ebae1b682c07cfe4dfb7
KonH <konhit@gmail.com>
2025-06-19 07:45:26 +0200

    Spawn prefabs considering DI

--- Assets/Scripts/UnityComponents/PrefabSpawner.cs
+++ Assets/Scripts/UnityComponents/PrefabSpawner.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
+using VContainer;
+using VContainer.Unity;
 
 namespace UnityComponents {
 	public class PrefabSpawner : MonoBehaviour {
 		[SerializeField] GameObject prefab = null!;
 		[SerializeField] GameObject root = null!;
 
+		IObjectResolver _objectResolver = null!;
+
+		[Inject]
+		public void Construct(IObjectResolver objectResolver) {
+			_objectResolver = objectResolver;
+		}
+
 		public void Spawn() {
-			Instantiate(prefab, root.transform);
+			_objectResolver.Instantiate(prefab, root.transform);
 		}
 	}
 }
