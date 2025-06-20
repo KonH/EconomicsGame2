83fb23f944f933383e29c77d47eb505ea3b6b957
KonH <konhit@gmail.com>
2025-06-20 21:49:43 +0200

    Validation fixes

--- Assets/Scripts/UnityComponents/UI/WindowController.cs
+++ Assets/Scripts/UnityComponents/UI/WindowController.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 using JetBrains.Annotations;
+using Common;
 
 namespace UnityComponents.UI {
 	[RequireComponent(typeof(Animator))]
 	public sealed class WindowController : MonoBehaviour {
 		static readonly int HideHash = Animator.StringToHash("Hide");
-		private Animator _animator;
 
-		private void Awake() {
+		Animator? _animator;
+
+		void Awake() {
 			_animator = GetComponent<Animator>();
 		}
 
 		public void Hide() {
+			if (!this.Validate(_animator)) {
+				return;
+			}
 			_animator.SetBool(HideHash, true);
 		}
 
