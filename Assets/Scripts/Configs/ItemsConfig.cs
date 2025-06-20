0052183862bb6c69569b7dda25c659af0991d7d8
KonH <konhit@gmail.com>
2025-06-20 21:38:39 +0200

    Post fix

--- Assets/Scripts/Configs/ItemsConfig.cs
+++ Assets/Scripts/Configs/ItemsConfig.cs
@@ -1,30 +1,25 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Configs {
 	[CreateAssetMenu(fileName = "ItemsConfig", menuName = "Configs/ItemsConfig")]
 	public sealed class ItemsConfig : ScriptableObject {
 		[SerializeField] ItemConfig[] items = Array.Empty<ItemConfig>();
-		private Dictionary<string, ItemConfig> itemDictionary = new Dictionary<string, ItemConfig>();
+
+		Dictionary<string, ItemConfig> _itemDictionary = new();
 
 		public ItemConfig[] Items => items;
 
-		private void OnEnable() {
-			// Populate the dictionary when the object is initialized
-			itemDictionary = new Dictionary<string, ItemConfig>();
+		void OnEnable() {
+			_itemDictionary = new Dictionary<string, ItemConfig>();
 			foreach (var item in items) {
-				if (!itemDictionary.ContainsKey(item.Id)) {
-					itemDictionary[item.Id] = item;
-				}
+				_itemDictionary.TryAdd(item.Id, item);
 			}
 		}
 
 		public ItemConfig? GetItemById(string id) {
-			if (itemDictionary.TryGetValue(id, out var item)) {
-				return item;
-			}
-			return null;
+			return _itemDictionary.GetValueOrDefault(id);
 		}
 	}
 }
