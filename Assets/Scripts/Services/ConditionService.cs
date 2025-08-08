b7db778ec8bc1d4ee5b17b937cd402c178e617a9
KonH <konhit@gmail.com>
2025-08-09 01:09:13 +0200

    Fix spaces

--- Assets/Scripts/Services/ConditionService.cs
+++ Assets/Scripts/Services/ConditionService.cs
@@ -18,55 +18,55 @@ namespace Services {
 
 			var ordered = conditionTypes.OrderBy(t => t.FullName).ToArray();
 			for (var i = 0; i < ordered.Length; i++) {
- 				var id = i + 1;
- 				var type = ordered[i];
- 				_idToType[id] = type;
- 				_typeToId[type] = id;
- 			}
- 		}
+				var id = i + 1;
+				var type = ordered[i];
+				_idToType[id] = type;
+				_typeToId[type] = id;
+			}
+		}
 
- 		static IEnumerable<Type> CollectConditionTypes() {
- 			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
- 			foreach (var assembly in assemblies) {
- 				Type[] types;
- 				try {
- 					types = assembly.GetTypes();
- 				} catch (ReflectionTypeLoadException e) {
- 					types = e.Types.Where(t => t != null).Cast<Type>().ToArray();
- 				}
+		static IEnumerable<Type> CollectConditionTypes() {
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			foreach (var assembly in assemblies) {
+				Type[] types;
+				try {
+					types = assembly.GetTypes();
+				} catch (ReflectionTypeLoadException e) {
+					types = e.Types.Where(t => t != null).Cast<Type>().ToArray();
+				}
 
- 				foreach (var type in types) {
- 					if (!type.IsValueType || type.IsPrimitive) {
- 						continue;
- 					}
- 					if (type.GetCustomAttribute<ConditionAttribute>() == null) {
- 						continue;
- 					}
- 					yield return type;
- 				}
- 			}
- 		}
+				foreach (var type in types) {
+					if (!type.IsValueType || type.IsPrimitive) {
+						continue;
+					}
+					if (type.GetCustomAttribute<ConditionAttribute>() == null) {
+						continue;
+					}
+					yield return type;
+				}
+			}
+		}
 
- 		public Type GetConditionType(int conditionId) {
- 			return _idToType[conditionId];
- 		}
+		public Type GetConditionType(int conditionId) {
+			return _idToType[conditionId];
+		}
 
- 		public int GetConditionId(Type conditionType) {
- 			return _typeToId[conditionType];
- 		}
+		public int GetConditionId(Type conditionType) {
+			return _typeToId[conditionType];
+		}
 
- 		public void AddCondition<T>(Entity entity, T condition) where T : struct {
- 			entity.Add(condition);
- 			var id = GetConditionId(typeof(T));
- 			entity.Add(new ConditionAdded { conditionId = id });
- 		}
+		public void AddCondition<T>(Entity entity, T condition) where T : struct {
+			entity.Add(condition);
+			var id = GetConditionId(typeof(T));
+			entity.Add(new ConditionAdded { conditionId = id });
+		}
 
- 		public void RemoveCondition<T>(Entity entity) where T : struct {
- 			if (entity.Has<T>()) {
- 				entity.Remove<T>();
- 			}
- 			var id = GetConditionId(typeof(T));
- 			entity.Add(new ConditionRemoved { conditionId = id });
- 		}
- 	}
+		public void RemoveCondition<T>(Entity entity) where T : struct {
+			if (entity.Has<T>()) {
+				entity.Remove<T>();
+			}
+			var id = GetConditionId(typeof(T));
+			entity.Add(new ConditionRemoved { conditionId = id });
+		}
+	}
 }
