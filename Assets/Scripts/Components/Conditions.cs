5beeb492fd909aab22abcdee9aadf42908d34d18
KonH <konhit@gmail.com>
2025-08-08 23:22:04 +0200

    Update implementation

--- Assets/Scripts/Components/Conditions.cs
+++ Assets/Scripts/Components/Conditions.cs
@@ -1,5 +1,20 @@
+using System;
+
 namespace Components
 {
+	public class ConditionAttribute : Attribute {}
+
+	[OneFrame]
+	public struct ConditionAdded {
+		public int conditionId;
+	}
+
+	[OneFrame]
+	public struct ConditionRemoved {
+		public int conditionId;
+	}
+
+	[Condition]
 	[Persistent]
 	public struct Hungry {
 		public float healthDecreaseValue;
--- Assets/Scripts/Configs/StatsConfig.cs
+++ Assets/Scripts/Configs/StatsConfig.cs
@@ -7,9 +7,11 @@ using Common;
 namespace Configs {
 	[Serializable]
 	public sealed class HungerConfig {
+		[SerializeField] private float _hungerIncreaseValue = 0.1f;
 		[SerializeField] [Range(0, 1)] private float _startAffectingHealthPercent = 0.5f;
 		[SerializeField] private float _healthDecreaseValue = 1f;
 
+		public float HungerIncreaseValue => _hungerIncreaseValue;
 		public float StartAffectingHealthPercent => _startAffectingHealthPercent;
 		public float HealthDecreaseValue => _healthDecreaseValue;
 	}
